ee5a7837e2edf23251fdbf3cdfd0886d2767af1d
Kasey Cushing <kasey.cushing@gmail.com>
2020-12-16 20:55:54 -0500

    pulls lists of employees and roles from db and offers as choices in inquirer

--- develop/queries/updateQueries.js
+++ develop/queries/updateQueries.js
@@ -10,81 +10,77 @@ let connection = mysql.createConnection({
   database: "employeesDB",
 });
 
-connection.connect((err, res) => {
-  if (err) throw err;
-  //ftn call here
-});
-
 function updateRole() {
   console.log("inside updateRole ftn");
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        message: "What is the employee's first name?",
-        name: "empFirst",
-      },
-      {
-        type: "input",
-        message: "What is the employee's last name?",
-        name: "empLast",
-      },
-      {
-        type: "input",
-        message: "What is the employee's new role?",
-        name: "newRole",
-      },
-    ])
-    .then((reply) => {
-      console.log("inside response from updateRole inquirer");
 
-      connection.query(
-        "SELECT * FROM employees FULL JOIN roles on role_id = roles.id",
-        (err, res) => {
+  connection.query("SELECT * FROM employees", (ers, rep) => {
+    if (ers) throw ers;
+    const makeNamesArr = function () {
+      let namesArr = [];
+      for (let i = 0; i < rep.length; i++) {
+        const addName =
+          rep[i].first_name + " " + rep[i].last_name + " " + rep[i].id;
+        namesArr.push(addName);
+      }
+      return namesArr;
+    };
+
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          message: "Please select an employee:",
+          name: "empName",
+          choices: makeNamesArr(),
+        },
+      ])
+      .then((ans) => {
+        console.log("inside .then for names arr inq");
+
+        connection.query("SELECT * FROM roles", (err, resps) => {
           if (err) throw err;
-          console.log("inside conn.query for updateRole");
-          console.table(res);
 
-          let checkRole = capitalize(reply.newRole);
-          let empFirst = capitalize(reply.empFirst);
-          let empLast = capitalize(reply.empLast);
-
-          console.log(checkRole, empLast, empFirst);
-
-          //filter for role id
-          const findRoleId = function () {
-            for (let i = 0; i < res.length; i++) {
-              if (res[i].title == checkRole) {
-                console.log("the " + checkRole + " id is " + res[i].role_id);
-                return res[i].role_id;
-              }
+          const makeRolesArr = function () {
+            let rolesArr = [];
+            for (let i = 0; i < resps.length; i++) {
+              const addRole = resps[i].title + " " + resps[i].id;
+              rolesArr.push(addRole);
             }
+            return rolesArr;
           };
 
-          console.log(findRoleId());
-          //filter for employee id
-          const findEmpId = function () {
-            for (let j = 0; j < res.length; j++) {
-              if (
-                res[j].first_name == empFirst &&
-                res[j].last_name == empLast
-              ) {
-                console.log("the employee name is " + empFirst + " " + empLast);
-                console.log("the emp id is " + res[j].id);
-                return res[j].id;
-              }
-            }
-          };
-          console.log(findEmpId());
-          updateRoleIdToDb(findEmpId(), findRoleId());
-        }
-      );
-    });
+          inquirer
+            .prompt([
+              {
+                type: "list",
+                message: "What is the employee's new role?",
+                name: "newRole",
+                choices: makeRolesArr(),
+              },
+            ])
+            .then((reply) => {
+              console.log("inside response from updateRole inquirer");
+              console.log(reply);
+
+              //filter for role id
+              const findRoleId = parseInt([...reply.newRole].pop());
+              const findEmpId = parseInt([...ans.empName].pop());
+
+              console.log(findRoleId);
+              //filter for employee id
+
+              console.log(findEmpId);
+              updateRoleIdToDb(findEmpId, findRoleId);
+            });
+        }); //end names query
+      });
+  });
 }
 
 function updateRoleIdToDb(empId, roleId) {
   const query = "UPDATE employees SET role_id = ? WHERE id = ?";
   const idsArr = [roleId, empId];
+  console.log(idsArr);
   connection.query(query, idsArr, (err) => {
     if (err) throw err;
     else {
