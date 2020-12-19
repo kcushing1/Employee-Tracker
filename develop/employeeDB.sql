c217fc6b0daa0c33735343430ce3d62d876beec7
Kasey Cushing <kasey.cushing@gmail.com>
2020-12-19 17:51:54 -0500

    update db file

--- develop/employeeDB.sql
+++ develop/employeeDB.sql
@@ -34,17 +34,19 @@ values
 
 insert into roles (title, salary, dept_id)
 values
-("Executive Chef", 60000.00,1), 
-("Director of Housekeeping", 56800.00, 2), 
-("Manager", 34000, 3), 
+("Cook", 40600.00,1), 
+("Housekeeping", 41800.00, 2), 
+("Manager", 34800, 3), 
 ("Engineer", 60000, 4), 
-("Sales", 46000, 3);
+("Sales", 46200, 3);
 
 insert into employees (first_name, last_name, role_id, manager_id)
 values 
-("Frodo", "Baggins", 1, 4), 
-("Harry", "Potter", 3,4), 
+("Frodo", "Baggins", 1, 3), 
+("Harry", "Potter", 3,3), 
 ("Leslie", "Knope", 3, null),
-("William", "Shakespeare", 4,4),
-("Peter", "Rabbit", 2,2)
-
+("William", "Shakespeare", 4,2),
+("Peter", "Rabbit", 2,2),
+("Veronica", "Mars",4,3),
+("Darth","Vader",1,3),
+("Sam","Adams",5,3)
