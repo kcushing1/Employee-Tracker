47a91674db61ca1c15c0006082015b973a1c0adf
Kasey Cushing <kasey.cushing@gmail.com>
2020-12-18 19:05:40 -0500

    remove console.logs, add notes

--- develop/queries/budgetQuery.js
+++ develop/queries/budgetQuery.js
@@ -11,10 +11,11 @@ let connection = mysql.createConnection({
 });
 
 function viewBudget() {
-  console.log("inside viewBudget ftn");
-
+  //query for departments array
   connection.query("SELECT * FROM departments", (err, resp) => {
     if (err) throw err;
+
+    //produce departments list
     const deptArr = function () {
       let deptsArr = [];
       for (let i = 1; i < resp.length; i++) {
@@ -23,7 +24,7 @@ function viewBudget() {
       }
       return deptsArr;
     };
-    console.log(deptArr());
+
     inquirer
       .prompt([
         {
@@ -34,37 +35,34 @@ function viewBudget() {
         },
       ])
       .then((ansDep) => {
-        console.log("inside .then for choose dept inq");
-
         const deptId = getId(ansDep.chosenDept);
-        console.log("dept id is " + deptId);
 
+        //query for all salaraies in chosen department
         connection.query(
           "SELECT salary FROM roles WHERE dept_id = ?",
           deptId,
           (ers, res) => {
             if (ers) throw ers;
-            console.log("inside connection to roles where deptid is");
 
+            //create an array of all salaries
             const salariesArr = function () {
               let salaryArr = [];
               for (let i = 0; i < res.length; i++) {
                 const addSalary = res[i].salary;
                 salaryArr.push(addSalary);
               }
-              //console.log(rolesArr);
               return salaryArr;
             };
-            console.log(salariesArr());
 
             const budget = addSalaries(salariesArr());
 
+            //add all salaries
             function addSalaries(arr) {
               return arr.reduce((accum, next) => {
                 return (accum += next);
               });
             }
-            console.log("the total dept budget is " + budget);
+            console.log("The total department budget is $ " + budget);
           }
         );
       });
